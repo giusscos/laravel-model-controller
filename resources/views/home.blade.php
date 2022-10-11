707005f95c1b7d407289795a05a81afe6d07b060
giusscos <giu.cosenza08@gmail.com>
2022-10-11 15:15:24 +0200

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('metaTitle', 'Home - Movies')
+
+@section('content')
+    <h1>
+        Ciao Movies
+    </h1>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{@asset('./css/app.css')}}">
+    <title>
+        @yield('metaTitle')
+    </title>
+</head>
+<body>
+
+    {{-- Header --}}
+
+    {{-- Main Content --}}
+    <main>
+        @yield('content')
+    </main>
+
+    {{-- Footer --}}
+</body>
+</html>
